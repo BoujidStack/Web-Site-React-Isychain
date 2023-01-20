ac59301a09f4c2dde61b808fb730e2d4f590b092
starj <starj1122x@gmail.com>
2023-01-20 10:52:54 +0100

    rollback styles

--- .next/static/tEizyiy3XeUCacX8Sogxr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/679-487e853527d4f8ed.js","static/css/6bd00b9303a89319.css","static/chunks/pages/index-7529e84fa56ae933.js"],"/_error":["static/chunks/pages/_error-350d9d86556f6d87.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
