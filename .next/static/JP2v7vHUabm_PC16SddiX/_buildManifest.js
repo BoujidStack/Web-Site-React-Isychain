f62ce4d83c7249763de22c67cea044c1d490570e
unknown <starlabman>
2023-06-15 14:43:35 +0000

    all is fine in this code , you can push and deploy them

---
+++ .next/static/JP2v7vHUabm_PC16SddiX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/679-047ad70aa22e69ff.js","static/css/234033825164e34a.css","static/chunks/pages/index-939004ad5a336f2e.js"],"/_error":["static/chunks/pages/_error-350d9d86556f6d87.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
